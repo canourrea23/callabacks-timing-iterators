a270e0ed6dd5bc43779e2b5b3eac10690c2df945
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-10-22 09:36:12 -0500

    hello

---
+++ checknumber.js
@@ -0,0 +1,32 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    } 
+}
+console.log(checkNumber(5)); 
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0){
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(7));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'postive';
+    } else if (num < 0){
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(-5));
---
+++ hello.js
@@ -0,0 +1,32 @@
+function sayHello(name) {
+    let msg = 'Hello,' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019')); // feel free to change the string being passed
+
+
+
+function sayHello(name) {
+    let msg = 'Hello,' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('2020')); // feel free to change the string being passed
+
+
+function sayHello(name) {
+    let msg = 'Hello,' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Carolina')); // feel free to change the string being passed
+
+
+function sayHello(name) {
+    let msg = 'Hello,' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('World'));
+
